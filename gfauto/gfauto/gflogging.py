cca3769bef24390abf7ea06add9fc9f27a255d96
Paul Thomson <paulthomson@google.com>
2019-07-06 12:38:01 +0100

    Fix copyright headers

--- gfauto/gfauto/gflogging.py
+++ gfauto/gfauto/gflogging.py
@@ -9,14 +9,12 @@
 #     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-
-
-# See the License for the specific language governing permissions and
-# limitations under the License.
-from pathlib import Path
-
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
 from typing import List, TextIO
 
 from .util import file_read_text
